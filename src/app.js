22e7b7a4dff85bdb1a30237c6316eff61c73c6dc
Anderson Reis Souto <andersonpensephp@hotmail.com>
2017-11-15 23:13:39 -0200

    Implementacao koa request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import Koa from 'koa';
+import cors from 'koa2-cors';
 import renderNunjucks from './lib/render';
 import router from './routes';
 
 const app = new Koa();
 
+app.key = ['foo'];
+
 app
+    .use(cors({
+        allowMethods: ['GET', 'POST', 'DELETE']
+    }))
     .use(renderNunjucks)
     .use(router.routes());
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,27 @@
 import KoaRouter from 'koa-router';
+import request from 'koa2-request';
 
 const router = new KoaRouter();
 
-router.get('/', async (ctx) => {
-    await ctx.render('index', {title: 'Nova Home'})
+router.get('/', async (ctx) =>  {
+    const options = {
+        url: 'https://jsonplaceholder.typicode.com/users',
+        method: 'get',     
+        headers: { 
+            'User-Agent': 'request',
+            'charset': 'UTF-8'
+        }
+    }
+    let response =  await request(options);
+    let data = JSON.parse(response.body);
+
+    await ctx.render('index', {infos: data});
+});
+
+router.get('/users/:name', async (ctx) => {
+    let name = ctx.params.name;
+    ctx.cookies.set('name', 'tobi');
+    await ctx.render('users', {name: `Olá ${name}`});
 });
 
 export default router;
